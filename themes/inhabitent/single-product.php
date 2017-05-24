0420bcafb4d9999814539bda027e1c3822ed73bb
Sonia <soniasophiaw@gmail.com>
2017-05-23 21:31:15 -0700

    Single Products Page Styled

--- themes/inhabitent/single-product.php
+++ themes/inhabitent/single-product.php
@@ -5,24 +5,49 @@
  * @package Inhabitent_Theme
  */
 
+
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+  <div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
       <div class="container">
+        <?php while ( have_posts() ) : the_post(); ?>
 
-		<?php while ( have_posts() ) : the_post(); ?>
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+          <div class="product-image-container">
+  
+          <?php if ( has_post_thumbnail() ) : ?>
+          <?php the_post_thumbnail( 'large' ); ?>
+          <?php endif; ?>
+          </div>
 
-    <h2 class="price">$<?php echo CFS()->get( 'price' ); ?></h2>
+        <div class="product-content-container">
+          <header class="entry-header">
+          <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+          </header><!-- .entry-header -->
 
-			<?php get_template_part( 'template-parts/content', 'single' ); ?>
+        <div class="entry-content">
+          <h2 class="price">$<?php echo CFS()->get( 'price' ); ?></h2>
+          <?php the_content(); ?>
 
-      
+        <div class="social-buttons">
+          <button type="button" class="sm-button"><i class="fa fa-facebook"></i> Like</button>
+          <button type="button" class="sm-button"><i class="fa fa-twitter"></i> Tweet</button>
+          <button type="button" class="sm-button"><i class="fa fa-pinterest"></i> Pin</button>
 
-		<?php endwhile; // End of the loop. ?>
-
-      </div>
-		</main><!-- #main -->
-	</div><!-- #primary -->
+        <?php
+          wp_link_pages( array(
+            'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
+            'after' => '</div>',
+          ) );
+        ?>
+  <?php endwhile; // End of the loop. ?>
+        </div>
+    </div><!-- .entry-content -->
+  </div>
+</article><!-- #post-## -->
+</main>
+</div>
+</div>
 
 <?php get_footer(); ?>
